8e6e0bf4e2b94c3e6fd1f14f1995dda30e7a62c4
LiubovErm <105315327+LiubovErm@users.noreply.github.com>
2023-03-04 12:24:25 +0200

    fix

--- models/contact.js
+++ models/contact.js
@@ -34,7 +34,7 @@ const addSchema = Joi.object({
 });
 
 const updateFavoriteSchema = Joi.object({
-    favorite: Joi.boolean().required().message("missing field favorite"),
+    favorite: Joi.boolean().required().messages({"any.required": "missing field favorite"}),
 });
 
 const schemas = {
