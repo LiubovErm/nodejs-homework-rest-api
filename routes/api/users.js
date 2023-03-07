4e69abc317af5418bdd05f5d50b94d37c1381b67
LiubovErm <105315327+LiubovErm@users.noreply.github.com>
2023-03-07 18:00:16 +0200

    fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,23 @@ const { schemas } = require("../../models/user");
 const { validation, ctrlWrapper, auth, upload } = require("../../middlewares");
 const { users: ctrl } = require("../../controllers");
 
+router.post(
+  "/register",
+  validation(schemas.registerUpSchema),
+  ctrlWrapper(ctrl.register)
+);
+
+router.get(
+  "/login",
+  validation(schemas.loginSchema),
+  ctrlWrapper(ctrl.login)
+);
+
+router.post(
+  "/logout",
+  auth,
+  ctrlWrapper(ctrl.logout));
+
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 
 router.patch(
